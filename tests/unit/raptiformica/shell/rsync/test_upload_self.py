0456647eaa4e4a9b1396a9982653652639d14e6f
Rick van de Loo <rickvandeloo@gmail.com>
2016-08-27 20:16:22 +0200

    upload config from user cache to remote hosts
    instead of from the project dir like previously

--- tests/unit/raptiformica/shell/rsync/test_upload_self.py
+++ tests/unit/raptiformica/shell/rsync/test_upload_self.py
@@ -1,4 +1,7 @@
-from raptiformica.settings import PROJECT_DIR, INSTALL_DIR
+from mock import call
+
+from raptiformica.settings import INSTALL_DIR, MUTABLE_CONFIG
+from raptiformica.settings import PROJECT_DIR
 from raptiformica.shell.rsync import upload_self
 from tests.testcase import TestCase
 
@@ -6,42 +9,58 @@ from tests.testcase import TestCase
 class TestUploadSelf(TestCase):
     def setUp(self):
         self.log = self.set_up_patch('raptiformica.shell.rsync.log')
-        self.execute_process = self.set_up_patch(
-            'raptiformica.shell.execute.execute_process'
+        self.upload = self.set_up_patch(
+            'raptiformica.shell.rsync.upload', return_value=0
         )
-        self.process_output = (0, 'standard out output', '')
-        self.execute_process.return_value = self.process_output
+        self.create_remote_raptiformica_cache = self.set_up_patch(
+            'raptiformica.shell.rsync.create_remote_raptiformica_cache',
+            return_value=0
+        )
+        self.create_remote_raptiformica_cache.return_value = 0
 
     def test_upload_self_logs_uploading_self_message(self):
         upload_self('1.2.3.4', port=22)
 
         self.assertTrue(self.log.info.called)
 
-    def test_upload_self_runs_upload_command(self):
+    def test_upload_self_uploads_project(self):
         upload_self('1.2.3.4', port=22)
 
-        expected_upload_command = [
-            '/usr/bin/env', 'rsync', '-L', '-avz', PROJECT_DIR,
-            'root@1.2.3.4:{}'.format(INSTALL_DIR), '--exclude=var/machines',
-            '--exclude', '*.pyc', '-e',
-            'ssh -p 22 '
-            '-oStrictHostKeyChecking=no '
-            '-oUserKnownHostsFile=/dev/null'
+        expected_calls = [
+            call(PROJECT_DIR, INSTALL_DIR, host='1.2.3.4', port=22),
+            call(MUTABLE_CONFIG, "$HOME/.raptiformica.d", host='1.2.3.4', port=22)
         ]
-        self.execute_process.assert_called_once_with(
-            expected_upload_command,
-            buffered=False,
-            shell=False
+        self.assertCountEqual(self.upload.mock_calls, expected_calls)
+
+    def test_upload_self_creates_remote_raptiformica_cache_directory(self):
+        upload_self('1.2.3.4', port=22)
+
+        self.create_remote_raptiformica_cache.assert_called_once_with(
+            '1.2.3.4', port=22
         )
 
-    def test_upload_self_raises_error_when_uploading_self_failed(self):
-        self.process_output = (1, '', 'standard error output')
-        self.execute_process.return_value = self.process_output
-
-        with self.assertRaises(RuntimeError):
-            upload_self('1.2.3.4', port=22)
-
-    def test_upload_self_returns_command_exit_code(self):
+    def test_upload_self_returns_true_if_success(self):
         ret = upload_self('1.2.3.4', port=22)
 
-        self.assertEqual(ret, 0)
+        self.assertTrue(ret)
+
+    def test_upload_returns_false_if_project_upload_fails(self):
+        self.upload.side_effect = [1, 0]
+
+        ret = upload_self('1.2.3.4', port=22)
+
+        self.assertFalse(ret)
+
+    def test_upload_returns_false_if_creating_remote_cache_directory_failed(self):
+        self.create_remote_raptiformica_cache.return_value = 1
+
+        ret = upload_self('1.2.3.4', port=22)
+
+        self.assertFalse(ret)
+
+    def test_upload_returns_false_if_syncing_mutable_config_failed(self):
+        self.upload.side_effect = [0, 1]
+
+        ret = upload_self('1.2.3.4', port=22)
+
+        self.assertFalse(ret)
