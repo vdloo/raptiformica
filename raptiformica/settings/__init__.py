33c6af551bb48fba8aecec47aebc58b80286a813
Rick van de Loo <rickvandeloo@gmail.com>
2016-08-27 13:48:50 +0200

    put mutable_config in user homedir

--- raptiformica/settings/__init__.py
+++ raptiformica/settings/__init__.py
@@ -1,4 +1,4 @@
-from os.path import dirname, realpath, join
+from os.path import dirname, realpath, join, expanduser
 
 PROJECT_DIR = join(dirname(dirname(dirname(realpath(__file__)))))
 INSTALL_DIR = '/usr/etc/'
@@ -7,7 +7,7 @@ EPHEMERAL_DIR = join(PROJECT_DIR, 'var')
 SCRIPTS_DIR = join(PROJECT_DIR, 'scripts')
 MACHINES_DIR = join(EPHEMERAL_DIR, 'machines')
 BASE_CONFIG = join(PROJECT_DIR, '.base_config.json')
-MUTABLE_CONFIG = join(PROJECT_DIR, 'mutable_config.json')
+MUTABLE_CONFIG = join(expanduser("~"), '.mutable_config.json')
 MODULES_DIR = join(PROJECT_DIR, 'modules')
 
 CJDNS_DEFAULT_PORT = 4863
