871b0f6cb4c6f145254545845ea036b93fe4de65
Rick van de Loo <rickvandeloo@gmail.com>
2016-09-01 09:08:48 +0200

    load user modules from ~/.raptiformica.d/modules

--- tests/unit/raptiformica/settings/load/test_load_modules.py
+++ tests/unit/raptiformica/settings/load/test_load_modules.py
@@ -1,3 +1,5 @@
+from mock import call
+
 from raptiformica.settings.load import load_modules
 from tests.testcase import TestCase
 
@@ -7,6 +9,7 @@ class TestLoadModules(TestCase):
         self.load_module_configs = self.set_up_patch(
             'raptiformica.settings.load.load_module_configs'
         )
+        self.load_module_configs.side_effect = [[1, 2, 3], [4, 5, 6]]
         self.compose_module_prototypes = self.set_up_patch(
             'raptiformica.settings.load.compose_module_prototypes'
         )
@@ -23,42 +26,44 @@ class TestLoadModules(TestCase):
         self.compose_types.side_effect = lambda *args: args
 
     def test_load_modules_loads_module_configs(self):
-        load_modules(modules_dir='/tmp/a/modules/dir')
+        load_modules(modules_dirs=('/tmp/a/modules/dir', '/tmp/a/nother/modules/dir'))
 
-        self.load_module_configs.assert_called_once_with(
-            modules_dir='/tmp/a/modules/dir'
-        )
+        expected_calls = [
+            call('/tmp/a/modules/dir'),
+            call('/tmp/a/nother/modules/dir')
+        ]
+        self.assertCountEqual(self.load_module_configs.mock_calls, expected_calls)
 
     def test_load_modules_composes_module_prototypes(self):
-        load_modules(modules_dir='/tmp/a/modules/dir')
+        load_modules(modules_dirs=('/tmp/a/modules/dir', '/tmp/a/nother/modules/dir'))
 
         self.compose_module_prototypes.assert_called_once_with(
-            self.load_module_configs.return_value
+            [1, 2, 3, 4, 5, 6]
         )
 
     def test_load_modules_finds_module_name_in_module_configs_recursively(self):
-        load_modules(modules_dir='/tmp/a/modules/dir')
+        load_modules(modules_dirs=('/tmp/a/modules/dir', '/tmp/a/nother/modules/dir'))
 
         self.find_key_in_module_configs_recursively.assert_called_once_with(
-            self.load_module_configs.return_value, 'module_name'
+            [1, 2, 3, 4, 5, 6], 'module_name'
         )
 
     def test_load_modules_returns_loaded_modules(self):
-        ret = load_modules(modules_dir='/tmp/a/modules/dir')
+        ret = load_modules(modules_dirs=('/tmp/a/modules/dir', '/tmp/a/nother/modules/dir'))
 
         self.assertEqual(
                 ret,
                 {
                     'value1': (
-                        self.load_module_configs.return_value,
+                        [1, 2, 3, 4, 5, 6],
                         'value1'
                     ),
                     'value2': (
-                        self.load_module_configs.return_value,
+                        [1, 2, 3, 4, 5, 6],
                         'value2'
                     ),
                     'value3': (
-                        self.load_module_configs.return_value,
+                        [1, 2, 3, 4, 5, 6],
                         'value3'
                     ),
                     'module_prototype': self.compose_module_prototypes.return_value
