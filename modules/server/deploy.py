37d32011bb900a0b155c40200e939bfb55eb762e
Rick van de Loo <rickvandeloo@gmail.com>
2016-10-08 21:27:09 +0200

    create temp loopback key in module def
    not during provisioning runtime

--- modules/server/deploy.py
+++ modules/server/deploy.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
+from raptiformica.log import setup_logging
 from raptiformica.shell.consul import ensure_consul_installed
 from raptiformica.shell.cjdns import ensure_cjdns_installed
-from raptiformica.shell.execute import run_command
 
 
 def main():
@@ -10,15 +10,9 @@ def main():
     scripts for the tools required by raptiformica itself.
     :return None:
     """
+    setup_logging()
     # todo: refactor this so that the shell commands below run on the local
     # host directly, instead of SSHing to the localhost and running it there
-    run_command(
-        "cat /dev/zero | ssh-keygen -f loopback -q -N '' > /dev/null 2>&1; "
-        "eval $(ssh-agent -s); ssh-add loopback; "
-        "cat loopback.pub >> ~/.ssh/authorized_keys",
-        shell=True,
-        buffered=False,
-    )
     ensure_cjdns_installed('localhost')
     ensure_consul_installed('localhost')
 
