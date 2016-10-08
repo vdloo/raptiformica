3bdc3f96736cfad29f8f74f83cf68ccd9d1cb00d
Rick van de Loo <rickvandeloo@gmail.com>
2016-10-08 21:07:42 +0200

    add loopback key
    run provisioning commands over SSH on the local host for now

--- modules/server/deploy.py
+++ modules/server/deploy.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from raptiformica.shell.consul import ensure_consul_installed
 from raptiformica.shell.cjdns import ensure_cjdns_installed
+from raptiformica.shell.execute import run_command
 
 
 def main():
@@ -9,6 +10,15 @@ def main():
     scripts for the tools required by raptiformica itself.
     :return None:
     """
+    # todo: refactor this so that the shell commands below run on the local
+    # host directly, instead of SSHing to the localhost and running it there
+    run_command(
+        "cat /dev/zero | ssh-keygen -f loopback -q -N '' > /dev/null 2>&1; "
+        "eval $(ssh-agent -s); ssh-add loopback; "
+        "cat loopback.pub >> ~/.ssh/authorized_keys",
+        shell=True,
+        buffered=False,
+    )
     ensure_cjdns_installed('localhost')
     ensure_consul_installed('localhost')
 
