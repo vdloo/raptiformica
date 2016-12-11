a71e8cbdf8d16edb8a0242d78aac5af8ce6e0ac8
Rick van de Loo <rickvandeloo@gmail.com>
2016-12-11 15:49:01 +0100

    add integration test for simple cluster
    boot 3 Docker instances in the same subnet, test if they can all reach each other using the overlay network
    
    1. test if consul consensus was established
    2. test if any instance can ping6 all registered cjdns peers
    3. test if data stored in the kv store from any instance is retrievable from any instance
    
    Running the simple cluster test:
    ```
    ./runtests -1 -i
    ```
    
    Note: this kills all your running raptiformica Dockers and deletes all
    cached Docker images. It also removed the entire ~/.raptiformica.d dir
    (including artifacts). The resources will be re-compiled every run for
    the first instance (for the second and third it will be cached).

--- runtests.sh
+++ runtests.sh
@@ -1,19 +1,33 @@
 #!/usr/bin/env bash
 set -e
 
-while getopts "1" opt; do
+while getopts "1i" opt; do
     case $opt in
         1) RUN_ONCE=1;;
+        i) INTEGRATION=1;;
     esac
 done
 
+[ -z $INTEGRATION ] && TEST_SUITE="unit" || TEST_SUITE="integration"
+[ -z $INTEGRATION ] && TIME_OUT="--process-timeout=30" || TIME_OUT="--process-timeout=1200"
+
+# Do not use the nose.proxy otherwise we can't run Popen with unbuffered output
+# fixes: nose.proxy.AttributeError: '_io.StringIO' object has no attribute 'buffer'
+[ -z $INTEGRATION ] && NO_CAPTURE="" || NO_CAPTURE="--nocapture"  
+
 shift $((OPTIND - 1))
 
-if [ -e "/proc/cpuinfo" ]; then
-    numprocs=$(cat /proc/cpuinfo  | grep processor | wc -l | cut -d ' ' -f 1)
-elif [ "x$(uname)" = "xDarwin" ]; then
-    numprocs=$(sysctl -n hw.ncpu)
+if [ -z $INTEGRATION ]; then
+    if [ -e "/proc/cpuinfo" ]; then
+        numprocs=$(cat /proc/cpuinfo  | grep processor | wc -l | cut -d ' ' -f 1)
+    elif [ "x$(uname)" = "xDarwin" ]; then
+        numprocs=$(sysctl -n hw.ncpu)
+    else
+        numprocs=1
+    fi
 else
+    echo "Testing sudo so we can start Dockers in the tests"
+    sudo echo "Could sudo. Ok!"
     numprocs=1
 fi
 
@@ -23,10 +37,10 @@ export PYTHONDONTWRITEBYTECODE=1
 find . -type f -name *.pyc -delete
 
 test_cmd="
-    echo 'Running raptiformica unit tests';
-    nosetests --processes=$numprocs;
+    echo 'Running raptiformica $TEST_SUITE tests';
+    $VIRTUAL_ENV/bin/nosetests --processes=$numprocs tests/$TEST_SUITE $TIME_OUT $NO_CAPTURE;
     echo 'Checking PEP8';
-    autopep8 -r --diff raptiformica;
+    $VIRTUAL_ENV/bin/autopep8 -r --diff raptiformica;
 "
 
 if [ -z $RUN_ONCE ]; then
