c986f1a8f501fa290295aac59e99a5571eca50b5
Rick van de Loo <rickvandeloo@gmail.com>
2017-03-19 16:22:30 +0100

    do not save IP in packaged box
    otherwise all machines spawned from packaged boxes will use the same IP and that won't work obviously

--- modules/compute/docker/package.sh
+++ modules/compute/docker/package.sh
@@ -26,6 +26,9 @@ MACHINE_UUID=$(grep .raptiformica.d/var/machines/docker/headless/ $LOG_FILE  | h
 # Change dir to the new check out
 cd $HOME/.raptiformica.d/var/machines/docker/headless/$MACHINE_UUID/modules/compute/docker/
 
+# Do not save an IP address in the packaged box
+sudo docker exec $(cat container_id) rm -f /etc/cjdroute.conf
+
 # Update the saved base image
 sudo docker commit $(cat container_id) raptiformica-baseimage
 
