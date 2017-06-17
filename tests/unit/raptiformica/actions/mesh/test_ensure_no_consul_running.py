30a26c19ca2aa72d3961b33c979d53314e857c24
Rick van de Loo <rickvandeloo@gmail.com>
2017-06-17 11:31:28 +0200

    gracefully terminate consul on restart
    On restart, consul should be gracefully stopped. Currently it just
    receives a kill (SIGTERM) which might leave the socket in TCP-WAIT
    and then Linux will wait for around 60 seconds to free the 8300 port
    for reuse. By sending a SIGINT instead consul will gracefully be
    stopped and the port will be ready for reuse immediately.
    
    Also see https://vincent.bernat.im/en/blog/2014-tcp-time-wait-state-linux

--- tests/unit/raptiformica/actions/mesh/test_ensure_no_consul_running.py
+++ tests/unit/raptiformica/actions/mesh/test_ensure_no_consul_running.py
@@ -22,7 +22,7 @@ class TestEnsureNoConsulRunning(TestCase):
         expected_command = "ps aux | grep [c]onsul | awk '{print $2}' | " \
                            "xargs --no-run-if-empty -I {} " \
                            "sh -c \"grep -q docker /proc/{}/cgroup && " \
-                           "grep -qv docker /proc/1/cgroup || kill {}\""
+                           "grep -qv docker /proc/1/cgroup || kill -SIGINT {}\""
         self.run_command_print_ready.assert_called_once_with(
             expected_command,
             shell=True,
@@ -47,9 +47,19 @@ class TestEnsureNoConsulRunning(TestCase):
         )
         self.assertIn(
             "-I {} sh -c \"grep -q docker /proc/{}/cgroup && "
-            "grep -qv docker /proc/1/cgroup || kill {}\"",
+            "grep -qv docker /proc/1/cgroup || kill -SIGINT {}\"",
             expected_command,
             'Should only kill processes not in Docker containers unless '
             'running inside a Docker, those could have their own raptiformica '
             'instances running'
+        )
+        self.assertIn(
+            "kill -SIGINT",
+            expected_command,
+            'Should gracefully kill consul. The default kill signal is '
+            'SIGTERM. Consul does not shut down gracefully on SIGTERM, '
+            'only on SIGINT. If it does not shut down gracefully there '
+            'is a chance that the socket will enter a TCP-WAIT state '
+            'after which Linux has a timeout of around 60 seconds before '
+            'the port becomes available again'
         )
