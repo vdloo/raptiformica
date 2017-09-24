163f472176b9ba4f3c7d4ad79b9df3514b521807
Rick van de Loo <rickvandeloo@gmail.com>
2017-09-24 17:21:32 +0200

    move systemd unit file to /usr/lib/systemd/system/
    not pretty but good enough for now

--- tests/unit/raptiformica/actions/mesh/test_configure_raptiformica_conf.py
+++ tests/unit/raptiformica/actions/mesh/test_configure_raptiformica_conf.py
@@ -1,4 +1,5 @@
-from unittest.mock import mock_open, ANY
+from functools import partial
+from unittest.mock import mock_open, ANY, call
 
 from raptiformica.actions.mesh import configure_raptiformica_conf
 from tests.testcase import TestCase
@@ -12,12 +13,15 @@ class TestConfigureRaptiformicaConf(TestCase):
         self.exists.return_value = False
         self.mock_open = mock_open()
         self.set_up_patch('raptiformica.actions.mesh.open', self.mock_open)
+        self.run_command_print_ready = self.set_up_patch(
+            'raptiformica.actions.mesh.run_command_print_ready'
+        )
 
     def test_configure_raptiformica_conf_checks_if_path_exists(self):
         configure_raptiformica_conf()
 
         self.exists.assert_called_once_with(
-           '/etc/systemd/system/multi-user.target.wants'
+           '/usr/lib/systemd/system/'
         )
 
     def test_configure_raptiformica_conf_does_not_write_to_service_file(self):
@@ -33,3 +37,24 @@ class TestConfigureRaptiformicaConf(TestCase):
         self.mock_open().write.assert_called_once_with(
             ANY
         )
+
+    def test_configure_conf_enables_oneshot_if_systemd_and_new_unit_file(self):
+        self.exists.side_effect = (True, False)
+
+        configure_raptiformica_conf()
+
+        expected_calls = map(
+            partial(call, shell=True, buffered=False),
+            ("systemctl daemon-reload",
+             "systemctl enable raptiformica")
+        )
+        self.assertCountEqual(
+            expected_calls, self.run_command_print_ready.mock_calls
+        )
+
+    def test_configure_conf_does_not_enable_oneshot_if_systemd_and_existing_unit_file(self):
+        self.exists.side_effect = (True, True)
+
+        configure_raptiformica_conf()
+
+        self.assertFalse(self.run_command_print_ready.called)
