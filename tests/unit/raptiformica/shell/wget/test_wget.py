bb3d56ab73d3646cbd999db88ffe15fa5ea41dff
Rick van de Loo <rickvandeloo@gmail.com>
2018-02-03 17:29:22 +0100

    force ipv4 wgets
    in case outbound ipv6 is not possible
    ```
    [root@grid1 ~]# wget https://releases.hashicorp.com/consul/1.0.2/consul_1.0.2_linux_amd64.zip
    --2018-02-03 17:27:07--  https://releases.hashicorp.com/consul/1.0.2/consul_1.0.2_linux_amd64.zip
    Loaded CA certificate '/etc/ssl/certs/ca-certificates.crt'
    Resolving releases.hashicorp.com (releases.hashicorp.com)... 2a04:4e42:9::439, 151.101.37.183
    Connecting to releases.hashicorp.com (releases.hashicorp.com)|2a04:4e42:9::439|:443...
    
    ^C
    
    [root@grid1 ~]# wget https://releases.hashicorp.com/consul/1.0.2/consul_1.0.2_linux_amd64.zip -4
    --2018-02-03 17:28:00--  https://releases.hashicorp.com/consul/1.0.2/consul_1.0.2_linux_amd64.zip
    Loaded CA certificate '/etc/ssl/certs/ca-certificates.crt'
    Resolving releases.hashicorp.com (releases.hashicorp.com)... 151.101.37.183
    Connecting to releases.hashicorp.com (releases.hashicorp.com)|151.101.37.183|:443... connected.
    HTTP request sent, awaiting response... 200 OK
    Length: 11098577 (11M) [application/zip]
    Saving to: 'consul_1.0.2_linux_amd64.zip'
    
    consul_1.0.2_linux_amd64.zip                                100%[==========================================================================================================================================>]  10.58M  33.0MB/s    in 0.3s
    
    2018-02-03 17:28:00 (33.0 MB/s) - 'consul_1.0.2_linux_amd64.zip' saved [11098577/11098577]
    ```

--- tests/unit/raptiformica/shell/wget/test_wget.py
+++ tests/unit/raptiformica/shell/wget/test_wget.py
@@ -26,7 +26,7 @@ class TestWget(TestCase):
             '-o', 'UserKnownHostsFile=/dev/null',
             '-o', 'PasswordAuthentication=no',
             'root@1.2.3.4', '-p', '22',
-            'wget', '-nc', 'https://www.example.com/some_url.zip'
+            'wget', '-4', '-nc', 'https://www.example.com/some_url.zip'
         ]
         self.execute_process.assert_called_once_with(
             expected_command,
