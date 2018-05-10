8699d4f78f55f6a5bce3f4a5b72efe99b02fa646
Rick van de Loo <rickvandeloo@gmail.com>
2018-05-10 13:36:53 +0200

    ensure artifacts directory
    even if there are no newly generated artifacts on the remote server

--- tests/unit/raptiformica/settings/load/test_write_config_mapping.py
+++ tests/unit/raptiformica/settings/load/test_write_config_mapping.py
@@ -1,9 +1,13 @@
+from raptiformica.settings import conf
 from raptiformica.settings.load import write_config_mapping
 from tests.testcase import TestCase
 
 
 class TestWriteConfigMapping(TestCase):
     def setUp(self):
+        self.ensure_directory = self.set_up_patch(
+            'raptiformica.settings.load.ensure_directory'
+        )
         self.write_json = self.set_up_patch(
             'raptiformica.settings.load.write_json'
         )
@@ -15,6 +19,19 @@ class TestWriteConfigMapping(TestCase):
         self.data = {
             'the': 'data'
         }
+
+    def test_write_config_ensures_artifact_directory(self):
+        write_config_mapping(self.data, 'a_config_file.json')
+
+        # Even though we only need the ABS_CACHE_DIR to
+        # exist here we still create the deeper level
+        # artifacts dir because that will be required
+        # later as well. TODO: find a better suited place
+        # to ensure the artifacts directory or make it not
+        # a hard requirement for download_artifacts
+        self.ensure_directory.assert_called_once_with(
+            conf().USER_ARTIFACTS_DIR
+        )
 
     def test_write_config_gets_config_cache_lock(self):
         write_config_mapping(self.data, 'a_config_file.json')
